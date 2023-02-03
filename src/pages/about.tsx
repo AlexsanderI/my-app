03989e97348ae983a35d47cd3fe236abfec06517
Your Name <y.y.oleksander@gmail.com>
2023-02-03 16:24:57 +0200

    intership

---
+++ src/pages/about.tsx
@@ -0,0 +1,76 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+export default function About(data: any) {
+  const [ID, setId] = useState('');
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    // let posts = JSON.stringify(data);
+
+    setPost(data.data.filter((post: any) => post.id === ID));
+  }, [data.data, ID]);
+
+  return (
+    <>
+      <h1>About</h1>
+      <Link href='/'>{`Hi`}</Link>
+      <p>
+        <button onClick={() => setId('1')}>1</button>
+      </p>
+      <p>
+        <button onClick={() => setId('2')}>2</button>
+      </p>
+      <p>
+        <button onClick={() => setId('3')}>3</button>
+      </p>
+      <p>
+        <button onClick={() => setId('4')}>4</button>
+      </p>
+
+      {ID !== '' && <div>{JSON.stringify(post)}</div>}
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    'https://62840a40a48bd3c40b6a3c4f.mockapi.io/api/v1/tasks'
+  );
+  let data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+const name = '1';
+
+// export function getServerSideProps(context: any) {
+//   console.log(context.resolvedUrl);
+
+//   fetch('https://62840a40a48bd3c40b6a3c4f.mockapi.io/api/v1/tasks');
+//   // .then((res) => res.json())
+//   // .then((res) => JSON.stringify(res))
+//   // .then((res) => console.log(res));
+//   return {
+//     props: { message: `Welcome ${context.resolvedUrl}` },
+//   };
+// }
+
+// export async function getServerSideProps() {
+//   // const [tasks, setTasks] = useState([] as any);
+//   let tasks: any = [];
+//   // useEffect(() => {
+//   fetch('https://62840a40a48bd3c40b6a3c4f.mockapi.io/api/v1/tasks')
+//     .then((res) => res.json())
+//     .then((res) => JSON.stringify(res))
+//     .then((res) => tasks.push(res));
+//   // });
+
+//   console.log(tasks);
+
+//   return {
+//     props: { message: tasks },
+//   };
+// }
